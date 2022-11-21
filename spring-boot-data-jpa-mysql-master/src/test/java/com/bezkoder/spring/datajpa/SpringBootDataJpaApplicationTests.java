e62c48526d52ef0c9a66df869327e9b19ecf674f
Sab_Berry <101462706+Sabarinathan33@users.noreply.github.com>
2022-11-21 10:15:02 +0530

    Add files via upload

---
+++ spring-boot-data-jpa-mysql-master/src/test/java/com/bezkoder/spring/datajpa/SpringBootDataJpaApplicationTests.java
@@ -0,0 +1,15 @@
+package com.bezkoder.spring.datajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class SpringBootDataJpaApplicationTests {
+
+	
+	@Test
+	void contextLoads() {
+	}
+
+}
